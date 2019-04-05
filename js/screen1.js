1af611b2e1281227afa16774a8a943a8e6a9ba90
NancyLSH <1052567432@aq.com>
2019-04-05 13:50:28 +0800

    can move

--- js/screen1.js
+++ js/screen1.js
@@ -1,14 +1,38 @@
+
+
 function open_close()
 {
     src = document.getElementById("barrage_button").getAttribute("src");
     if(src == './picture/5-1.png'){
-        document.getElementById("barrage_button").src='./picture/5-2.png'
+        document.getElementById("barrage_button").src='./picture/5-2.png';
+        $(".barrage").css("visibility","hidden");
     }else{
-        document.getElementById("barrage_button").src='./picture/5-1.png'
+        document.getElementById("barrage_button").src='./picture/5-1.png';
+        $(".barrage").css("visibility","visible");
     }
    // button.src="./picture/5-1.png"?button.src="./picture/5-2.png": button.src="./picture/5-1.png"     
 }
-function input(){
-    var text = $("#textinput").val();
-    
+
+function messaction(ppp) {
+    ppp.addClass("changmessage");
 }
+
+function messstop(ppp) {
+    ppp.remove();
+}
+
+function pushtext(){
+    var message=$("#textinput").val();
+    if(message.length>0){
+        $(".barrage").append("<div class='barragetext'>"+message+"</div>");
+        $("#textinput").val("");
+        var ppp= $(".barragetext").last();        
+        setTimeout(function(){
+            messaction(ppp);
+        },0);
+        setTimeout(function(){
+            messstop(ppp);
+        },8000);
+    }
+};
+
