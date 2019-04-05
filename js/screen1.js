683a03dd8c02ecf3eb183b4d3f302eda842cd252
NancyLSH <1052567432@aq.com>
2019-04-05 21:49:49 +0800

    done.

--- js/screen1.js
+++ js/screen1.js
@@ -1,16 +1,50 @@
+var socket = new WebSocket("ws://echo.websocket.org")
+//const socket = new WebSocket('ws://localhost:80');
+var message = $("#textinput").val()
+var username = "username"
 
+socket.onopen = function () {
+    console.log("WebSocket is open now.");
+};
 
-function open_close()
-{
-    src = document.getElementById("barrage_button").getAttribute("src");
-    if(src == './picture/5-1.png'){
-        document.getElementById("barrage_button").src='./picture/5-2.png';
-        $(".barrage").css("visibility","hidden");
-    }else{
-        document.getElementById("barrage_button").src='./picture/5-1.png';
-        $(".barrage").css("visibility","visible");
+function btn() {
+    var message = $("#textinput").val();
+    if (message.length > 0) {
+        socket.send({
+            "username": username,
+            "message": message
+        });
+        socket.onmessage = function (data) {
+            if (data.errcode == 0) {                //成功发送
+                mesend();
+            } if(data.errcode == 1) {                //未登录用户
+                $("#textinput").val("");
+                alert("您还未登录噢")
+            }if(data.errcode == 2){                //敏感词过滤
+                $("#textinput").val("");
+                alert("想清楚再发")
+            }
+        }
+    } else {
+        alert("donnt have message")
     }
-   // button.src="./picture/5-1.png"?button.src="./picture/5-2.png": button.src="./picture/5-1.png"     
+};
+
+socket.onmessage = function(data){
+    var name = data.username
+    var mess = data.message
+    $("#com_part").append("<p class='head name'>" + name + "</p>" + "<p class='introtext comm'>" + mess +"</p>")
+}
+function open_close() {
+    src = document.getElementById("barrage_button").getAttribute("src");
+    if (src == './picture/5-1.png') {
+        document.getElementById("barrage_button").src = './picture/5-2.png';
+        $(".barrage").css("visibility", "hidden");
+    } else {
+        document.getElementById("barrage_button").src = './picture/5-1.png';
+        $(".barrage").css("visibility", "visible");
+    }
+    // button.src="./picture/5-1.png"?button.src="./picture/5-2.png": button.src="./picture/5-1.png"     
 }
 
 function messaction(ppp) {
@@ -21,18 +55,14 @@ function messstop(ppp) {
     ppp.remove();
 }
 
-function pushtext(){
-    var message=$("#textinput").val();
-    if(message.length>0){
-        $(".barrage").append("<div class='barragetext'>"+message+"</div>");
-        $("#textinput").val("");
-        var ppp= $(".barragetext").last();        
-        setTimeout(function(){
-            messaction(ppp);
-        },0);
-        setTimeout(function(){
-            messstop(ppp);
-        },8000);
-    }
-};
-
+function mesend() {
+    $(".barrage").append("<div class='barragetext'>" + message + "</div>");
+    $("#textinput").val("");
+    var ppp = $(".barragetext").last();
+    setTimeout(function () {
+        messaction(ppp);
+    }, 0);
+    setTimeout(function () {
+        messstop(ppp);
+    }, 8000);
+}
