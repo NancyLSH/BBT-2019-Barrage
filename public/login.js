79ac446740b964f19b21f32b0533003e884a48f7
NancyLSH <1052567432@aq.com>
2019-04-14 22:43:42 +0800

    4.3

--- public/login.js
+++ public/login.js
@@ -31,30 +31,31 @@ $(function(){
                 window.location.href = ("http://134.175.124.192/BBT-2019-Barrage/index.html");
             }
         }})
-        
-        // $.post("http://111.230.183.100/barrage/login.php",{"username":name,"password":pwd},function (data) { 
-        //     if(data.errcode != 0){
-        //         $(".errmsg").html("");
-        //         $(".errmsg").css("display","block");
-        //         $(".errmsg").append(data.msg);
-        //     }else{
-        //         window.location.href = ("http://134.175.124.192/BBT-2019-Barrage/index.html");
-        //     }
-        //  })
     })
+
     $("#signup").click(function(e){
         e.preventDefault();
         var name = $("#siname").val()
         var pwd = $("#sipwd").val()
         var agpwd = $("#agpwd").val()
-        $.post("http://111.230.183.100/barrage/register.php",{"username":name,"password":pwd,"checkpwd":agpwd},function(data){
+        $.ajax({
+            url:"http://111.230.183.100/barrage/register.php",
+            type:"post",
+            data:{"username":name,"password":pwd,"checkpwd":agpwd},
+            dataType:"json",
+            xhrFields: {
+                withCredentials: true
+            },
+            crossDomain: true,
+            contentType: "application/x-www-form-urlencoded",
+        success:function(data){
             if(data.errcode != 0){
                 $(".errmsg").html("");
                 $(".errmsg").css("display","block");
                 $(".errmsg").append(data.msg);
             }else{
-                window.location.href = ("http://134.175.124.192/BBT-2019-Barrage/index.html")
+                window.location.href = ("http://134.175.124.192/BBT-2019-Barrage/index.html");
             }
-        })
+        }})    
     })    
 })
