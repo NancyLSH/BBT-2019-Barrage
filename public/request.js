be5912af126a6852d88c578b83165794a0172367
NancyLSH <1052567432@aq.com>
2019-04-15 02:15:55 +0800

    4.3

--- public/request.js
+++ public/request.js
@@ -14,7 +14,6 @@ $("#query").click(function(){
 
     $.post("http://111.230.183.100/barrage/keySearch.php",{"key":key},function(res){
         $("#contain").html("");
-
         for(i=0;i<res.length;i++){
             $("#contain").append("<div id='" + res[i]["id"] + "' class='com_usr flex'>" + "<input type='image' class='imghead' src='./public/picture/5-15.png' style='width:35%'></div>")
             $("#" + res[i]["id"]).append("<div class='column flex' id='com_part'>" + "<p class='head name'>" + res[i]["username"] + "</p>" + "<p class='introtext comm'>" + res[i]["record"] + "</p>"+
