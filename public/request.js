3a7011a780eab4853f8e2ee9a4e6f7e0cb2d3a35
NancyLSH <1052567432@aq.com>
2019-04-14 23:49:30 +0800

    4.3

--- public/request.js
+++ public/request.js
@@ -4,7 +4,7 @@ $.get("http://111.230.183.100/barrage/record.php",function(res){
     for(i=0;i<res.length;i++){
         console.log(res[i]["time"])
         $(".contain").append("<div id='" + res[i]["id"] + "' class='com_usr'>" + "<input type='image' class='imghead' src='./public/picture/5-15.png' style='width:100px;'></div>")
-        $("'#" + res[i]["id"] + "'").append("<div class='com_part'>" + "<p class='head name'>" + res[i]["username"] + "</p>" + "<p class='introtext comm'>" + res[i]["record"] + "</p>"+
+        $("#" + res[i]["id"]).append("<div class='com_part'>" + "<p class='head name'>" + res[i]["username"] + "</p>" + "<p class='introtext comm'>" + res[i]["record"] + "</p>"+
         "<p class='time'>" + res[i]["time"] + "</p></div>" )
     }
 })
