9e4c75e4dcf4a3e5458b3ad00d18af07c173d5d4
NancyLSH <1052567432@aq.com>
2019-04-14 22:36:22 +0800

    4.3

--- public/login.js
+++ public/login.js
@@ -1,22 +1,3 @@
-$.ajax({
-    url:"http://111.230.183.100/barrage/username.php",
-    type:"get",
-    data:"",
-    dataType:"json",
-    xhrFields: {
-        withCredentials: true
-    },
-    crossDomain: true,
-    contentType: "application/x-www-form-urlencoded",
-        success:function(res){
-        usrname = res.data.username;
-        usrid = res.data.userID;
-    },
-    error:function(res){
-        console.log(res.msg)
-    }
-})
-
 
 function btn() {
     $(".signup").css("display","none");
@@ -31,7 +12,17 @@ $(function(){
         e.preventDefault()
         var name = $("#name").val();
         var pwd = $("#password").val();
-        $.post("http://111.230.183.100/barrage/login.php",{"username":name,"password":pwd},function (data) { 
+        $.ajax({
+            url:"http://111.230.183.100/barrage/login.php",
+            type:"post",
+            data:{"username":name,"password":pwd},
+            dataType:"json",
+            xhrFields: {
+                withCredentials: true
+            },
+            crossDomain: true,
+            contentType: "application/x-www-form-urlencoded",
+        function(data){
             if(data.errcode != 0){
                 $(".errmsg").html("");
                 $(".errmsg").css("display","block");
@@ -39,7 +30,17 @@ $(function(){
             }else{
                 window.location.href = ("http://134.175.124.192/BBT-2019-Barrage/index.html");
             }
-         })
+        }})
+        
+        // $.post("http://111.230.183.100/barrage/login.php",{"username":name,"password":pwd},function (data) { 
+        //     if(data.errcode != 0){
+        //         $(".errmsg").html("");
+        //         $(".errmsg").css("display","block");
+        //         $(".errmsg").append(data.msg);
+        //     }else{
+        //         window.location.href = ("http://134.175.124.192/BBT-2019-Barrage/index.html");
+        //     }
+        //  })
     })
     $("#signup").click(function(e){
         e.preventDefault();
