37b3ee1c6d8259430ac21abece50eb0534ddd955
NancyLSH <1052567432@aq.com>
2019-04-14 23:24:16 +0800

    4.3

--- public/screen1.js
+++ public/screen1.js
@@ -75,9 +75,9 @@ socket.onopen = function () {
 };
 
 socket.onmessage = function (data) {
-    console.log(data)
-    console.log(JSON.parse(data.data).record)
-    mesend(JSON.parse(data).record, type,m)
+    console.log(data)                        //???
+    console.log(JSON.parse(data).data)
+    mesend(JSON.parse(data).data, type,m)
 }
 
 //发送弹幕模块
