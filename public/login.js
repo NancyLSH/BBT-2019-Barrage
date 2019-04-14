e8cfb1691a9f6be3739406db821c6e704248f689
NancyLSH <1052567432@aq.com>
2019-04-15 03:20:22 +0800

    4.3

--- public/login.js
+++ public/login.js
@@ -13,7 +13,7 @@ function btn_(){
     $(".sign").css("-webkit-box-shadow"," 0 0 11px rgba(44, 44, 44, 0.781)");
 }
 $(function(){
-    $(".contain").css("height",height*0.37)
+    $(".contain").css("height",height*0.32)
     $("#signin").click(function(e){
         e.preventDefault()
         var name = $("#name").val();
