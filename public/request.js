7353c12ae04979d95875c167ffdad371af03801c
NancyLSH <1052567432@aq.com>
2019-04-14 23:57:25 +0800

    4.3

--- public/request.js
+++ public/request.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 console.log("hhh")
 $.get("http://111.230.183.100/barrage/record.php",function(res){
     for(i=0;i<res.length;i++){
+        $("#contain").val("")
         console.log(res[i]["time"])
         $("#contain").append("<div id='" + res[i]["id"] + "' class='com_usr flex'>" + "<input type='image' class='imghead' src='./public/picture/5-15.png' style='width:35%'></div>")
         $("#" + res[i]["id"]).append("<div class='column flex' id='com_part'>" + "<p class='head name'>" + res[i]["username"] + "</p>" + "<p class='introtext comm'>" + res[i]["record"] + "</p>"+
@@ -12,6 +13,7 @@ $("#query").click(function(){
     var key = $("#query_input").val();
     $.post("http://111.230.183.100/barrage/keySearch.php",{"key":key},function(res){
         for(i=0;i<res.length;i++){
+            $("#contain").val("");
             $("#contain").append("<div id='" + res[i]["id"] + "' class='com_usr flex'>" + "<input type='image' class='imghead' src='./public/picture/5-15.png' style='width:35%'></div>")
             $("#" + res[i]["id"]).append("<div class='column flex' id='com_part'>" + "<p class='head name'>" + res[i]["username"] + "</p>" + "<p class='introtext comm'>" + res[i]["record"] + "</p>"+
             "<p class='time'>" + res[i]["time"] + "</p></div>" )
